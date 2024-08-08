c38c0df70623212583588acd0f2b6e05565c73c8
Ali <aliloloee82@gmail.com>
2024-08-08 12:38:15 +0200

    added celery, redis configs to settings.py

--- resc/resc/settings.py
+++ resc/resc/settings.py
@@ -77,6 +77,18 @@ AUTH_USER_MODEL = 'accounts.User'
 ASGI_APPLICATION = 'resc.asgi.application'
 
 
+# Internationalization
+# https://docs.djangoproject.com/en/5.0/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_TZ = True
+
+
 # A crucial step for working with postgis data on windows
 # GDAL Configuration and Installation on Windows for Django Projects
 # https://medium.com/@limeira.felipe94/gdal-configuration-and-installation-on-windows-for-django-projects-538171db5ccc
@@ -99,6 +111,41 @@ DATABASES = {
     }
 }
 
+# CACHE
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient"
+        },
+        "KEY_PREFIX": "caching"
+    }
+}
+
+
+# CELERY CONFIG
+CELERY_BROKER_URL = f'redis://127.0.0.1:6379/0'
+CELERY_RESULT_BACKEND = f'redis://127.0.0.1:6379/0'
+
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_ACCEPT_CONTENT = ['json', ]
+CELERY_RESULT_EXPIRES = timedelta(days=1)
+CELERY_TIMEZONE = TIME_ZONE
+CELERY_TASK_ALWAYS_EAGER = False
+CELERY_WORKER_PREFETCH_MULTIPLIER = 4
+
+# CHANNELS LAYER
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": ["redis://127.0.0.1:6379/2"],
+        },
+    },
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -117,18 +164,6 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
-
-# Internationalization
-# https://docs.djangoproject.com/en/5.0/topics/i18n/
-
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
-USE_I18N = True
-
-USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
